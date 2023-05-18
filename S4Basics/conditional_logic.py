ad9ec4ad318d9f3ef08bb8d7b6552779622b3ecc
BrentMRPalmer <bpalm015@uottawa.ca>
2023-05-17 20:21:25 -0400

    May 17

---
+++ S4Basics/conditional_logic.py
@@ -0,0 +1,11 @@
+# Conditional Logic
+
+is_old = True
+is_licensed = True
+
+if is_old and is_licensed:
+	print("You are old enough and licensed to drive")
+else:
+	print("You are not old enough or you are not licensed to drive")
+
+print("Thanks for checking!")
